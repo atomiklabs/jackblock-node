4870402ac0703122ef3083a3616a22aabe55c290
korzewski <korzewski@gmail.com>
2021-03-16 17:45:25 +0100

    add new bet feature

--- pallets/jackblock/src/tests.rs
+++ pallets/jackblock/src/tests.rs
@@ -2,8 +2,21 @@ use crate::{mock::*};
 use frame_support::{assert_ok};
 
 #[test]
-fn it_works_for_default_value() {
+fn add_new_bet_works() {
 	new_test_ext().execute_with(|| {
-		assert_ok!(JackBlock::example_extrinsic(Origin::signed(1)));
+		let session_id = JackBlock::session_id();
+		assert_eq!(JackBlock::bets(session_id), vec![]);
+
+		let (account_id, guess_numbers, bet) = (1, [0; crate::GUESS_NUMBERS_COUNT], 100);
+		
+		assert_ok!(JackBlock::add_new_bet(Origin::signed(account_id), guess_numbers, bet));
+		
+		let bet = crate::Bet {
+			account_id,
+			guess_numbers,
+			bet,
+		};
+
+		assert_eq!(JackBlock::bets(session_id), vec![bet]);
 	});
 }
