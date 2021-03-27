b91e45dba237245f6f078e7b439c451f1ada94ca
korzewski <korzewski@gmail.com>
2021-03-27 19:11:53 +0100

    add 3 public key pairs - but commented out

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -146,23 +146,46 @@ pub fn private_testnet_config() -> Result<ChainSpec, String> {
 			// Initial PoA authorities
 			vec![
 				(
-					sr25519::Public::from_slice(&hex!("8c95143789a7de6b1081b8de5d2882797f4b8d7e49b7d18bed2c9dfba0093749")).into(),
+					sr25519::Public::from_slice(&hex!("8c95143789a7de6b1081b8de5d2882797f4b8d7e49b7d18bed2c9dfba0093749")).into(), // node-0
 					ed25519::Public::from_slice(&hex!("3ba3259226032472d23bdf929e87ca64a67a0d6bae1590026f2878f390bfe46e")).into(),
 				),
 				(
-					sr25519::Public::from_slice(&hex!("1c012da62a15bc32bddcc446ead445e6e3db649718b75f7564e52110f9d16259")).into(),
+					sr25519::Public::from_slice(&hex!("1c012da62a15bc32bddcc446ead445e6e3db649718b75f7564e52110f9d16259")).into(), // node-1
 					ed25519::Public::from_slice(&hex!("d673017b1fe89ae315440ef77fbe126a5c73dfef550e51621cf464e9d4f2f77e")).into(),
 				),
+
+				// (
+				// 	sr25519::Public::from_slice(&hex!("f6de5cd7b974141f359efd875f56d261ceb1c7af567fc92787e6912d1ebe875b")).into(), // rafal
+				// 	ed25519::Public::from_slice(&hex!("8425afe90642a0aa98cccdf5d49404fe1141e34b98c30516b303df5b6f3e6f36")).into(),
+				// ),
+				// (
+				// 	sr25519::Public::from_slice(&hex!("286818e712a21ffb2efd5af0b38e1ddd5525cd03788b4f62f9a85429a9238078")).into(), // miko
+				// 	ed25519::Public::from_slice(&hex!("02a51935ea51f45a72f176e1f3f542fdbb790ccaf8f5b357faaa8b6882147024")).into(),
+				// ),
+				// (
+				// 	sr25519::Public::from_slice(&hex!("3e0d36d4f8e06c6f92a71acd57ea6d56c5b55347c8883a172a732ff0dd24211d")).into(), // tomek
+				// 	ed25519::Public::from_slice(&hex!("76961074863c9ff54eec83b534cb2e17f1316af5e692a5d1021603884128d2c9")).into(),
+				// ),
 			],
 			// Off-chain authorities
 			vec![
-				hex!("8c95143789a7de6b1081b8de5d2882797f4b8d7e49b7d18bed2c9dfba0093749").into(),
-				hex!("1c012da62a15bc32bddcc446ead445e6e3db649718b75f7564e52110f9d16259").into(),
+				hex!("8c95143789a7de6b1081b8de5d2882797f4b8d7e49b7d18bed2c9dfba0093749").into(), // node-0
+				hex!("1c012da62a15bc32bddcc446ead445e6e3db649718b75f7564e52110f9d16259").into(), // node-1
+
+				hex!("f6de5cd7b974141f359efd875f56d261ceb1c7af567fc92787e6912d1ebe875b").into(), // rafal
+				hex!("286818e712a21ffb2efd5af0b38e1ddd5525cd03788b4f62f9a85429a9238078").into(), // miko
+				hex!("3e0d36d4f8e06c6f92a71acd57ea6d56c5b55347c8883a172a732ff0dd24211d").into(), // tomek
 			],
 			// Sudo account
-			get_account_id_from_seed::<sr25519::Public>("Alice"),
+			sr25519::Public::from_slice(&hex!("8c95143789a7de6b1081b8de5d2882797f4b8d7e49b7d18bed2c9dfba0093749")).into(), // node-0
 			// Pre-funded accounts
-			vec![],
+			vec![
+				hex!("8c95143789a7de6b1081b8de5d2882797f4b8d7e49b7d18bed2c9dfba0093749").into(), // node-0
+				
+				hex!("f6de5cd7b974141f359efd875f56d261ceb1c7af567fc92787e6912d1ebe875b").into(), // rafal
+				hex!("286818e712a21ffb2efd5af0b38e1ddd5525cd03788b4f62f9a85429a9238078").into(), // miko
+				hex!("3e0d36d4f8e06c6f92a71acd57ea6d56c5b55347c8883a172a732ff0dd24211d").into(), // tomek
+			],
 			true,
 		),
 		// Bootnodes
