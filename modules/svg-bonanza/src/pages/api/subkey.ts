c2fb8d4f57e99c844085b6bb7f9a2cf15f22e872
Tomasz Kopacki <tomasz@kopacki.net>
2021-03-28 10:37:36 +1100

    feat(subkey): verify arbitrary payload
    This is a test of an approach that will be later taken to verify if the payload was signed by known authority. We will process requests coming from known authorities only.

--- modules/svg-bonanza/src/pages/api/subkey.ts
+++ modules/svg-bonanza/src/pages/api/subkey.ts
@@ -5,9 +5,11 @@ import {
   scryptEncode,
   blake2AsHex,
   keccakAsHex,
-  schnorrkelKeypairFromSeed
+  schnorrkelKeypairFromSeed,
+  signatureVerify
 } from '@polkadot/util-crypto';
 import { u8aToU8a } from '@polkadot/util';
+import { Keyring } from '@polkadot/keyring';
 
 export default async function subkey(
   req: NextApiRequest,
@@ -17,16 +19,27 @@ export default async function subkey(
 
   const mnemonic = mnemonicGenerate();
 
+  const keyring = new Keyring({
+     type: 'sr25519'
+  });
+
+  keyring.addFromMnemonic(mnemonic)
+
+  const keyPair = keyring.getPairs()[0];
+  const payload = 'message';
+  const signature =  keyPair.sign(u8aToU8a(payload), { withType: true });
+
   res
     .status(200)
     .json({
       message: 'Ok',
       mnemonic,
-      scrypt: scryptEncode(mnemonic),
-      keccak: keccakAsHex(mnemonic),
-      blake2: blake2AsHex(mnemonic),
-      keyPair: schnorrkelKeypairFromSeed(
-        u8aToU8a(mnemonic)
-      )
+      // scrypt: scryptEncode(mnemonic),
+      // keccak: keccakAsHex(mnemonic),
+      // blake2: blake2AsHex(mnemonic),
+      // keyPair,
+      signature,
+      payload,
+      isVerified: signatureVerify(`${payload}`, signature, keyPair.publicKey).isValid
     });
 }
