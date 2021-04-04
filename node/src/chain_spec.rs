32f3d608a41e5fbae031b5c88dcb8a43b0a97add
korzewski <korzewski@gmail.com>
2021-04-04 10:07:55 +0200

    remove JackBlockConfig from chain_spec.rs

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -1,7 +1,7 @@
 use sp_core::{Pair, Public, sr25519, ed25519};
 use node_template_runtime::{
 	AccountId, AuraConfig, BalancesConfig, GenesisConfig, GrandpaConfig,
-	SudoConfig, SystemConfig, WASM_BINARY, Signature, JackBlockConfig,
+	SudoConfig, SystemConfig, WASM_BINARY, Signature,
 };
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
 use sp_finality_grandpa::AuthorityId as GrandpaId;
@@ -54,10 +54,6 @@ pub fn development_config() -> Result<ChainSpec, String> {
 			vec![
 				authority_keys_from_seed("Alice"),
 			],
-			// Off-chain authorities
-			vec![
-				hex!["ae70ee67f0b51dacc666f0d17f46fbead307846682385f69bb1c9d5d77701616"].into(),
-			],
 			// Sudo account
 			get_account_id_from_seed::<sr25519::Public>("Alice"),
 			// Pre-funded accounts
@@ -103,11 +99,6 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 					sr25519::Public::from_slice(&hex!("1c012da62a15bc32bddcc446ead445e6e3db649718b75f7564e52110f9d16259")).into(), // node-1
 					ed25519::Public::from_slice(&hex!("d673017b1fe89ae315440ef77fbe126a5c73dfef550e51621cf464e9d4f2f77e")).into(),
 				),
-			],
-			// Off-chain authorities
-			vec![
-				hex!("8c95143789a7de6b1081b8de5d2882797f4b8d7e49b7d18bed2c9dfba0093749").into(), // node-0
-				hex!("1c012da62a15bc32bddcc446ead445e6e3db649718b75f7564e52110f9d16259").into(), // node-1
 			],
 			// Sudo account
 			sr25519::Public::from_slice(&hex!("8c95143789a7de6b1081b8de5d2882797f4b8d7e49b7d18bed2c9dfba0093749")).into(), // node-0
@@ -157,12 +148,6 @@ pub fn public_testnet_config() -> Result<ChainSpec, String> {
 					ed25519::Public::from_slice(&hex!("76961074863c9ff54eec83b534cb2e17f1316af5e692a5d1021603884128d2c9")).into(),
 				),
 			],
-			// Off-chain authorities
-			vec![
-				hex!("f6de5cd7b974141f359efd875f56d261ceb1c7af567fc92787e6912d1ebe875b").into(), // rafal
-				hex!("286818e712a21ffb2efd5af0b38e1ddd5525cd03788b4f62f9a85429a9238078").into(), // miko
-				hex!("3e0d36d4f8e06c6f92a71acd57ea6d56c5b55347c8883a172a732ff0dd24211d").into(), // tomek
-			],
 			// Sudo account
 			sr25519::Public::from_slice(&hex!("f6de5cd7b974141f359efd875f56d261ceb1c7af567fc92787e6912d1ebe875b")).into(), // node-0
 			// Pre-funded accounts
@@ -190,7 +175,6 @@ pub fn public_testnet_config() -> Result<ChainSpec, String> {
 fn testnet_genesis(
 	wasm_binary: &[u8],
 	initial_authorities: Vec<(AuraId, GrandpaId)>,
-	offchain_authorities: Vec<AccountId>, // TO BE REMOVED --------------------------
 	root_key: AccountId,
 	endowed_accounts: Vec<AccountId>,
 	_enable_println: bool,
@@ -214,9 +198,6 @@ fn testnet_genesis(
 		pallet_sudo: Some(SudoConfig {
 			// Assign network admin rights.
 			key: root_key,
-		}),
-		pallet_jackblock: Some(JackBlockConfig {
-			offchain_authorities,
 		}),
 	}
 }
